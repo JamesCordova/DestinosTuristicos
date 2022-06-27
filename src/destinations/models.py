cee475f968f99f9c854374c213bf8ae401adcead
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-26 20:15:34 -0500

    se creo la clase Destinations junato a sus campos en donde esta el ImageField

--- src/destinations/models.py
+++ src/destinations/models.py
@@ -1,3 +1,9 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    nombre = models.CharField(max_length = 100)
+    img = models.ImageField(upload_to = 'pics')
+    descripcion = models.TextField()
+    nombre = models.CharField(default = False)
