6bea78e67720a30bb21db42e5d749fc63d7a9325
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 14:55:16 -0500

    se definio la ruta para crear un destination, mandar el form de este

--- src/destinations/urls.py
+++ src/destinations/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     destinationsList,
     destinationsShow,
+    destinationsCreate,
 )
 
 urlpatterns = [
     path('', destinationsList, name="lista"),
     path('<int:myID>/', destinationsShow, name="descripcion"),
+    path('create/', destinationsCreate, name="crear"),
 ]
