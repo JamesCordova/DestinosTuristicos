890dea08f0f64d61af7daed4f1a63f3754fbaf03
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 14:20:47 -0500

    se agrego un objeto Destination, se corrigio la funcion que llama cuando se manda un id

--- src/destinations/urls.py
+++ src/destinations/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import destinationsList
+from .views import (
+    destinationsList,
+    destinationsShow,
+)
 
 urlpatterns = [
     path('', destinationsList, name="lista"),
-    path('<int:myID>/', destinationsList, name="descripcion"),
+    path('<int:myID>/', destinationsShow, name="descripcion"),
 ]
