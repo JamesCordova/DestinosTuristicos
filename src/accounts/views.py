ec1851315fa4675ab00bca1113ddf07df8861a28
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 18:21:39 -0500

    se definio el view, para que usara el UserForm que llama al login.html

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User,auth
+from .forms import UserForm
+
 
 # Create your views here.
+
+def login(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = auth.authenticate(**form.cleaned_data)
+    else:
+        print(form.errors)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
