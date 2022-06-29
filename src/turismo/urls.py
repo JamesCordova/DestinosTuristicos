e23f39ba3b1dc8265f54bb33769801a10f8d7b06
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 14:00:57 -0500

    la ruta fue corregida ahora se considera todo lo que tenga destinations como parte del path

--- src/turismo/urls.py
+++ src/turismo/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('destinations.urls')),
+    path('destinations/', include('destinations.urls')),
 ]
