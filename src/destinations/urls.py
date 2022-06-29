9216014573d82622b99ed76d255561b9133f7121
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 16:53:23 -0500

    se agrego la ruta a urls.py junto al view, se observa que los campos estan desordenados

--- src/destinations/urls.py
+++ src/destinations/urls.py
@@ -4,10 +4,12 @@ from .views import (
     destinationsList,
     destinationsShow,
     destinationsCreate,
+    destinationsEdit,
 )
 
 urlpatterns = [
     path('', destinationsList, name="lista"),
     path('<int:myID>/', destinationsShow, name="descripcion"),
     path('create/', destinationsCreate, name="crear"),
+    path('edit/<int:myID>/', destinationsEdit, name="editar"),
 ]
