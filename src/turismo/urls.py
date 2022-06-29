bffe21fd25e02b5644f3fd1df6e4761c5efd1ad7
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 18:32:37 -0500

    se incluyo los urls de accounts en el proyecto, urls.py

--- src/turismo/urls.py
+++ src/turismo/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('destinations/', include('destinations.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
