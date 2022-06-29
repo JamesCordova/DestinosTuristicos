4ec55825d7990bc27fd4c8670c2268c4fa0621e9
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 13:43:26 -0500

    se agrego el view que envia la pagina destinations

--- src/destinations/views.py
+++ src/destinations/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
+
+def destinationsList(request):
+    queryset = Destination.objects.all()
+    context = {
+        'objectList': queryset,
+    }
+    return render(request, 'destinations/destinations.html', context)
