12bdfb880eb30a0fcf81eb04fad8fa1fa1e5ef49
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 19:11:48 -0500

    se agrego el widget con el que se define un passwordinput y un placeholder por estetica

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 class RawUserLoginForm(forms.Form):
     username = forms.CharField(label='Usuario')
-    password = forms.CharField(label='Contraseña')
+    password = forms.CharField(label='Contraseña',
+    widget = forms.PasswordInput(
+        attrs = {
+            'placeholder': 'Ingrese su contraseña',
+        }
+    ))
