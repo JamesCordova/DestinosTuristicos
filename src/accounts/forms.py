80613c779a7e31824baa7a005136caba0c2972b6
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 20:23:20 -0500

    se corrigio las validaciones, se agrego una validacion al password2, para que funcionara correctamente, al mismo tiempo la forma en que se llamaba a esos valores

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -55,8 +55,15 @@ class RawUserRegisterForm(forms.Form):
         return username
     
     def clean_password1(self, *args, **kwargs):
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Las contraseñas no coinciden')
+        return password1
+
+    def clean_password2(self, *args, **kwargs):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError('Las contraseñas no coinciden')
         return password1
