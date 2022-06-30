1a9e3798a3e1f324622d6444e093e4a0bf8a6825
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 20:05:17 -0500

    se agrego al urls.py del app accounts las rutas para logout y register

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from accounts.views import login
+from accounts.views import login, logout, register
 
 app_name = 'accounts'
 urlpatterns = {
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('register/', register, name='register'),
 }
