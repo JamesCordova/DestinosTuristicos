61a9cc5c43d64b1add98d805cb5a2dd34c80a6ef
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-29 19:26:47 -0500

    se creo el form class del register user para ser utilizado en un view diferente

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -8,3 +8,41 @@ class RawUserLoginForm(forms.Form):
             'placeholder': 'Ingrese su contraseña',
         }
     ))
+
+class RawUserRegisterForm(forms.Form):
+    first_name = forms.CharField(label='Nombre',
+    widget = forms.TextInput(
+        attrs = {
+            'placeholder': 'Nombres',
+        }
+    ))
+    last_name = forms.CharField(label='Apellido',
+    widget = forms.TextInput(
+        attrs = {
+            'placeholder': 'Apellidos',
+        }
+    ))
+    username = forms.CharField(label='Usuario',
+    widget = forms.TextInput(
+        attrs = {
+            'placeholder': 'Nombre de usuario',
+        }
+    ))
+    email = forms.CharField(label='Correo Electrónico',
+    widget = forms.EmailInput(
+        attrs = {
+            'placeholder': 'Correo electrónico',
+        }
+    ))
+    password1 = forms.CharField(label='Contraseña',
+    widget = forms.PasswordInput(
+        attrs = {
+            'placeholder': 'Ingrese su contraseña',
+        }
+    ))
+    password2 = forms.CharField(label='Confirmar Contraseña',
+    widget = forms.PasswordInput(
+        attrs = {
+            'placeholder': 'Ingrese de nuevo su contraseña',
+        }
+    ))
