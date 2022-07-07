cf1005ab4edc59f60edd46fc5b2d34d6693b67d8
JamesCordova <jcordovas@unsa.edu.pe>
2022-07-07 07:43:20 -0500

    se reemplazo el metodo index por la clase view, que tambien hereda de un listview generico, en donde se establecio un nombre de template para evitar cambiar el nombre al archivo, ademas de cmabiar lo necesario en urls.py e incluso cambiar el nombre por el que se llamaba a la lista de objetos en el index.html, que ahora se llama object_list

--- src/travello/views.py
+++ src/travello/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from destinations.models import Destination
+from django.views.generic import (
+    ListView,
+)
 
 # Create your views here.
-
-def index(request):
-    destinations = Destination.objects.all()
-    context = {
-        'destinations': destinations,
-    }
-    return render(request, 'index.html', context)
+class Index(ListView):
+    model = Destination
+    template_name = "index.html"
+    
